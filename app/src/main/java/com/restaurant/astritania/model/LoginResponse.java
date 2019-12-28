b4c664a70f5f6699c869333b9b8776aab39f0506
astritaniatsani <astritania05@gmail.com>
2019-12-28 22:12:39 +0700

    UAS Praktikum Mobile Programming

---
+++ app/src/main/java/com/restaurant/astritania/model/LoginResponse.java
@@ -0,0 +1,32 @@
+package com.restaurant.astritania.model;
+
+import com.google.gson.annotations.Expose;
+
+public class LoginResponse {
+    @Expose
+    private Login data;
+    @Expose
+    private String message;
+    @Expose
+    private String status;
+    public Login getLogin() {
+        return data;
+    }
+    public void setLogin(Login data) {
+        this.data = data;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
+
